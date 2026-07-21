5d2217e9de9ea66b84311a1624815caf96ed97d6
Rebeca Félix <felixbeca3@gmail.com>
2026-07-21 19:41:24 -0300

    Implementa lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -65,4 +65,20 @@ export class ListaProdutos {
   }
   //! metodo para criar um estado de seleção com signal string | null
   produtoSelecionado = signal <string | null>(null);
+  //! metodo para criar um estado para carrinho com signal 
+  carrinho = signal <{nome: string; preco: number}[]>([]);
+  adicionarAoCarrinho(produto:{nome: string; preco: number}){
+    this.carrinho.update(listaAtual =>[...listaAtual, produto] 
+    );
+  }
+  //! totalProdutos = computed (( => this.produtos().Length);
+  //metodo para calcular a quantidade total de itens no carrinho
+  quantidadeCarrinho = computed (() => this.carrinho().length);
+// metodo para calcular o valor total dos itens do carrinho
+totalCarrinho = computed (() => {
+  return this.carrinho().reduce((total, item) =>
+  total + item.preco,0)});
+;//! valorTotal = computed(()=> {
+//! return this.produtos().reduce(total, item) =>
+//! total + item.preco,0)});
 }
