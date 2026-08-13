149c2e8a80f3385f647ccf353576ad3c6a4d1983
Rebeca Félix <felixbeca3@gmail.com>
2026-08-12 21:34:17 -0300

    refactor: ajusta carrinho de gerenciamento do carrinho

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
 @Injectable ({
     providedIn:'root'
 })
@@ -9,16 +13,16 @@ import { computed } from "@angular/core";
 export class CarrinhoService {
 
     //! Estado Global - Criado com Sucesso
-    private carrinho = signal <{nome: string; preco: number }[]>([]);
+    private carrinho = signal <ItemCarrinho[]>([]);
 
     //? Seleção
     itens = computed (()=> this.carrinho());
-    quantidadeItens = computed (()=> this.carrinho().length);
-    totalItens = computed (()=> 
-    this.carrinho().reduce((total, item) => total + item.preco,0));
+    quantidadeItens = computed (()=> this.carrinho().length); 
+    totalItens = computed (()=> this.carrinho().reduce((total, item) => total + item.preco,0));
+    carrinhoVazio = computed(() => this.carrinho().length === 0);
 
-// TODO: Ações
-adicionar(produto:{nome: string; preco: number}){
+// TODO: Ação Adicionar Produtos
+adicionar(produto:ItemCarrinho){
     this.carrinho.update(lista => [...lista, produto]);
 }
 // TODO: Ação de Limpeza
