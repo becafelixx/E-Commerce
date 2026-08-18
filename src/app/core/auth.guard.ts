b7aeef1663dfe9c12653027dfce7b2dba8cc1415
Rebeca Félix <felixbeca3@gmail.com>
2026-08-17 22:06:27 -0300

    feat: adiciona proteção de rotas para usuários autenticados

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn } from "@angular/router";
 import { AuthService } from "./services/auth.service";
-import { inject, Inject } from "@angular/core";
+import { inject, } from "@angular/core";
 import { Router } from "@angular/router";
 
 export const authGuard: CanActivateFn = () => {
