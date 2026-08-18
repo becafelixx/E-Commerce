83700f5a2c095c9554853d069733e8fa78e5f78c
Rebeca Félix <felixbeca3@gmail.com>
2026-08-17 22:05:39 -0300

    feat: atualiza configuração das rotas da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from "@angular/router";
 import { authGuard } from "./core/auth.guard";
+import { adminGuard } from "./core/services/admin.guard";
 
 export const routes: Routes = [
     {
-        path: '',
+        path: '', //! router para raiz localhost:4200/
         loadComponent: () => import('./features/home/home/home')
         .then((m) => m.Home),
     },
@@ -21,10 +22,26 @@ export const routes: Routes = [
     },
     {
         path: 'checkout',
+        canActivate: [authGuard],
         loadComponent: () =>
             import('./features/checkout/checkout/checkout')
         .then((m)=> m.Checkout),
             
+    }, 
+    {
+        path:'login',
+        loadComponent:() => import('./features/login/login/login').then((m) => m.Login),
+    },
+    {
+        path: 'admin',
+        canActivate: [adminGuard],
+        loadComponent: () =>
+            import('./features/admin/admin/admin').then((m) => m.Admin),
+    },
+    {
+        path:'acesso-negado',
+        loadComponent: () =>
+            import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado)
     },
     {
         path: '**',
