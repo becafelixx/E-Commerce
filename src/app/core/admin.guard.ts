461ae056bb273643f8ad45bca4172e84978e8ea7
Rebeca Félix <felixbeca3@gmail.com>
2026-08-19 22:01:46 -0300

    feat: adiciona guard de acesso administrativo

---
+++ src/app/core/admin.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+import { AuthFacade } from "./facades/auth.facade";
+
+export const adminGuard: CanActivateFn = () => {
+    const router = inject(Router)
+    const authFacade = inject(AuthFacade)
+    if(!authFacade.usuarioLogado()){
+        return router.createUrlTree(['/login']);
+    }
+    if(!authFacade.admin()){
+        return router.createUrlTree(['/acesso-negado']);
+    }
+
+return true;
+}
