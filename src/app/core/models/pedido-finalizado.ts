4b33c2dcaf3baa28202bf8405b9533379967adf1
Rebeca Félix <felixbeca3@gmail.com>
2026-08-21 20:22:08 -0300

    feat; adiciona modelo de dados para pedido finalizado

---
+++ src/app/core/models/pedido-finalizado.ts
@@ -0,0 +1,11 @@
+import { ItemCarrinho } from "./item-carrinho"
+
+
+export type PedidoFinalizado ={
+    codigo: number;
+    cliente: string;
+    email: string;
+    quantidadeItens: number;
+    total: number;
+    itens: ItemCarrinho[];
+}
